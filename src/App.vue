594fca1ca3996ab8b7f0b91082ed0f3584b6c2f1
GvansaKob <gvansa.kobesashvili@my-digital-school.org>
2025-04-01 12:26:45 +0200

    Add btn filter  "A-Z"

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,13 @@
     </button>
 
     <div v-if="pokemons.length > 0">
-      <input type="text" v-model="searchQuery" placeholder="Rechercher un Pokémon..." class="search-input" />
+      <div class="search-sort-container">
+        <input type="text" v-model="searchQuery" placeholder="Rechercher un Pokémon..." class="search-input" />
+
+        <button @click="toggleSortOrder" class="sort-button">
+          Trier {{ sortOrder === 'asc' ? 'Z-A' : 'A-Z' }}
+        </button>
+      </div>
 
       <Powers @filter="handleFilter" class="powers-container" />
 
@@ -24,6 +30,7 @@ import Powers from "./components/Powers.vue";
 const pokemons = ref([]);
 const selectedType = ref("");
 const searchQuery = ref("");
+const sortOrder = ref("asc");
 
 const fetchPokemon = async () => {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50");
@@ -45,18 +52,28 @@ const normalizeText = (text) => {
 };
 
 const filteredPokemons = computed(() => {
-  return pokemons.value.filter((pokemon) => {
-    const matchesType =
-      !selectedType.value ||
-      pokemon.types.some((type) => type.type.name === selectedType.value);
+  return pokemons.value
+    .filter((pokemon) => {
+      const matchesType =
+        !selectedType.value ||
+        pokemon.types.some((type) => type.type.name === selectedType.value);
 
-    const matchesSearch =
-      !searchQuery.value ||
-      normalizeText(pokemon.name).includes(normalizeText(searchQuery.value));
+      const matchesSearch =
+        !searchQuery.value ||
+        normalizeText(pokemon.name).includes(normalizeText(searchQuery.value));
 
-    return matchesType && matchesSearch;
-  });
+      return matchesType && matchesSearch;
+    })
+    .sort((a, b) => {
+      return sortOrder.value === "asc"
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name);
+    });
 });
+
+const toggleSortOrder = () => {
+  sortOrder.value = sortOrder.value === "asc" ? "desc" : "asc";
+};
 
 const handleFilter = (type) => {
   selectedType.value = type;
@@ -64,12 +81,29 @@ const handleFilter = (type) => {
 </script>
 
 <style scoped>
-.search-input {
+.search-sort-container {
   margin-top: 10px;
+}
+
+.search-input {
   padding: 8px;
-  width: 50%;
+  margin-right: 10px;
+  width: 25%;
   border: 1px solid #ccc;
   border-radius: 5px;
+}
+
+.sort-button {
+  padding: 8px 12px;
+  border: none;
+  background-color: rgb(11, 90, 164);
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.sort-button:hover {
+  background-color: #5798da;
+  color: white;
 }
 
 .powers-container {
